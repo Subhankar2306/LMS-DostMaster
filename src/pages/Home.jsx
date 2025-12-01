9f5528eada2671c7dc9edd8dddc5036c3362b05a
Subhankar <subhankardhal637@gmail.com>
2025-12-01 22:27:05 +0530

    Home Courses and Notification ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";  
 import Banner from "../components/Banner";
+import HomeCourse from "../components/HomeCourse";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+      <HomeCourse />
     </div>
   );
 };
