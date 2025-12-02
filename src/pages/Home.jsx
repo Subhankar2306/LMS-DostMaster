94b2dbf51959e4d0e010246693ddfa14c3735d3a
Subhankar <subhankardhal637@gmail.com>
2025-12-02 21:34:05 +0530

    Testimonial Completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";  
 import Banner from "../components/Banner";
 import HomeCourse from "../components/HomeCourse";
+import Testimonial from "../components/Testimonial";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Banner />
       <HomeCourse />
+      <Testimonial />
     </div>
   );
 };
